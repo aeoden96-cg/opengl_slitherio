d91d30540c4bb28f78cd0397c9169888b76b5eec
Mateo <mateo1559@gmail.com>
2021-12-11 01:47:50 +0100

    wip

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,8 @@
 // Kompajliranje:
 // g++ -o SimpleAnim SimpleAnim.cpp util.cpp -lGLEW -lGL -lGLU -lglut -lpthread
 
-#include <cstdlib>
 #include <iostream>
-
+#include <numeric>
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <glm/glm.hpp>
@@ -21,24 +20,23 @@
     #define LINUX_UBUNTU_SEGFAULT
 #endif
 
-#ifdef LINUX_UBUNTU_SEGFAULT
-    #include <pthread.h>
-#endif
+//#ifdef LINUX_UBUNTU_SEGFAULT
+//    #include <pthread.h>
+//#endif
 
 
-GLuint window;
+[[maybe_unused]] GLuint window;
 GLuint sub_width = 500, sub_height = 500;
 int WindowHeight;
 int WindowWidth;
 const double Xmin = 0.0, Xmax = 3.0;
 const double Ymin = 0.0, Ymax = 3.0;
-glm::vec2 mousePos(0,0);
 
 glm::mat4 projection;
-
-Shader s;
-Shader s_background;
-Slither sl(0,0,0.1f);
+glm::vec2 mousePos(0,0);
+Shader shader_snake;
+Shader shader_background;
+Slither snake(0, 0, 0.1f);
 
 struct Background{
     std::vector<GLfloat> points;
@@ -60,12 +58,13 @@ struct Background{
         return points;
     }
 };
-Background b;
+Background background;
+
 
 glm::mat4 addView(glm::mat4 model){
     glm::mat4 view = glm::lookAt(
-            sl.head()+glm::vec3(0,0,1),
-            sl.head(),
+            snake.head() + glm::vec3(0, 0, 1),
+            snake.head(),
             glm::vec3(0,1,0)
     );
     return view * model;
@@ -73,13 +72,13 @@ glm::mat4 addView(glm::mat4 model){
 glm::mat4 createMVPEye(glm::vec3 localEyePosition){
     glm::mat4 model(1.0f);
 
-    model = glm::translate (model, sl.head());
+    model = glm::translate (model, snake.head());
 
-    model = glm::rotate (model, sl.getDirection(),glm::vec3(0,0,1));
+    model = glm::rotate (model, snake.getDirection(), glm::vec3(0, 0, 1));
     model = glm::translate (model, localEyePosition);
 
     model = glm::rotate (model,
-                         glm::atan(mousePos.y,mousePos.x)-sl.getDirection(),
+                         glm::atan(mousePos.y,mousePos.x) - snake.getDirection(),
                          glm::vec3(0,0,1));
 
     return addView(model);
@@ -93,80 +92,112 @@ class Renderer{
 
 
 
-    void enableVA(int numOfInputAttributes) const{
-        glEnableVertexAttribArray(0);
-        if(numOfInputAttributes > 1)
-            glEnableVertexAttribArray(1);
-    }
-    void disableVA(int numOfInputAttributes) const{
-        glDisableVertexAttribArray(0);
-        if(numOfInputAttributes > 1)
-            glDisableVertexAttribArray(1);
+    static void enableVA(unsigned numOfInputAttributes) {
+        for (int i=0;i<numOfInputAttributes ; i++)
+            glEnableVertexAttribArray(i);
 
     }
-public:
-    Renderer(){
+    static void disableVA(unsigned numOfInputAttributes) {
+        for (int i=0;i<numOfInputAttributes ; i++)
+            glDisableVertexAttribArray(i);
+
     }
 
 
-    void setupData(const std::vector<GLfloat>& my_data,int positionWidth,int colorWidth){
+
+    /**
+     * Sends data to GPU.
+     * @param points All data points organised AS SHOWN in 'positions'.
+     * @param positions Shows how data points are organised by attribute.For example,
+     *  (1,3) means first attribute has 1 dimension, second has 3.
+     */
+    static void setupData(const std::vector<GLfloat>& points, const std::vector<int>& positions){
+
+        unsigned short W = std::accumulate(positions.begin(), positions.end(), 0);
+
         GLuint VAO;
         glGenVertexArrays(1, &VAO);
         glBindVertexArray(VAO);
         GLuint VBO;
         glGenBuffers(1, &VBO);
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*my_data.size(),  &my_data[0], GL_DYNAMIC_DRAW);
-        glVertexAttribPointer(
-                0,                                              // attribute 0.
-                positionWidth,                                  // size
-                GL_FLOAT,                                       // type
-                GL_FALSE,                                       // normalized?
-                sizeof(GLfloat)*(positionWidth+colorWidth),     // stride
-                (void*)nullptr                                  // array buffer offset
-        );
-
-        //color
-        if (colorWidth == 0) return;
-        glVertexAttribPointer(
-                1,                                              // attribute 1.
-                colorWidth,
-                GL_FLOAT,
-                GL_FALSE,
-                sizeof(GLfloat)*(positionWidth+colorWidth),
-                (const GLvoid *)(sizeof(GLfloat) * positionWidth)
-        );
+        glBufferData(GL_ARRAY_BUFFER, (unsigned)(sizeof(GLfloat) * points.size()), &points[0], GL_DYNAMIC_DRAW);
+        int i=0;
+        unsigned short formerAttribs=0;
+        for (auto& currentAttrib : positions)
+        {
+            glVertexAttribPointer(
+                    i,                                              //i.th attribute
+                    currentAttrib,                                  //num of dimensions for ith attrib
+                    GL_FLOAT,
+                    GL_FALSE,
+                    (int)(sizeof(GLfloat)*W),                            //width of one row
+                    (const GLvoid *)(sizeof(GLfloat) * formerAttribs)   //offset
+            );
+            formerAttribs+=currentAttrib;
+            i++;
+        }
     }
 
 
-    void draw(glm::mat4& mvp,int count,Shader& current_shader,int numOfInputAttributes){
+    /**
+     * Draws ONE instance of each of n='count' points.
+     * It also enables/disables each attribute for that data set.
+     * @param mvp
+     * @param count
+     * @param current_shader
+     * @param numOfInputAttributes
+     */
+    static void draw(const glm::mat4& mvp,unsigned count,Shader& current_shader,unsigned numOfInputAttributes){
         enableVA(numOfInputAttributes);
+
         current_shader.setMat4("MVP" ,&mvp[0][0]);
 
         if(!current_shader.isUsingTess())
-            glDrawArrays(GL_POINTS,0,count);
+            glDrawArrays(GL_POINTS,0,(int)count);
         else{
             glPatchParameteri(GL_PATCH_VERTICES,1);
-            glDrawArrays(GL_PATCHES,0,count);
+            glDrawArrays(GL_PATCHES,0,(int)count);
         }
+
         disableVA(numOfInputAttributes);
     }
-
-    void render(Shader& current_shader,
-                int positionWidth,
-                int colorWidth,
+public:
+    Renderer()= default;
+    /**
+     * Render points in this manner:
+     * for EVERY mvp in MVPs, draw ALL points from 'data'
+     * @param current_shader
+     * @param positions Show how attributes are organised. For example,
+     *  (1,3) means first attribute has 1 dimension, second has 3.
+     * @param data data organised AS SHOWN in 'positions'
+     * @param MVPs multiple MVP matrices,each represents instance of set of points
+     */
+    static void render(Shader& current_shader,
+                std::vector<int> positions,
                 const std::vector<GLfloat>& data,
-                glm::mat4& mvp){
+                std::vector<glm::mat4>& MVPs){
+
+
 
         current_shader.use();
-        this->setupData(data,positionWidth,colorWidth);
-        this->draw(mvp,data.size()/(positionWidth+colorWidth),current_shader,1 + !!colorWidth);
+
+        Renderer::setupData(data,positions);
+
+        int sum = std::accumulate(positions.begin(), positions.end(), 0);
+
+        for (const auto& mvp: MVPs){
+            Renderer::draw(mvp,
+                       data.size()/sum,   // = num of points to be rendered
+                       current_shader,
+                       positions.size());
+        }
+
+
     }
 
+
 };
-
-bool init_data();
-
 
 void glutPassiveMotionFunc(int x, int y ) {
 
@@ -183,48 +214,21 @@ void glutPassiveMotionFunc(int x, int y ) {
 
 }
 
-void myKeyboardFunc( unsigned char key, int x, int y )
-{
-	switch ( key ) {
-	case 'r':
-//		RunMode = 1-RunMode;		// Toggle to opposite value
-//		if ( RunMode==1 ) {
-//			glutPostRedisplay();
-//		}
-		break;
-	case 's':
-//		RunMode = 1;
-//		myDisplay();
-//		RunMode = 0;
-		break;
-    case 'w':
-        sl.hop();
-
-        break;
-    case 'd':
-        sl.add();
-        break;
-
-	case 27:	// Escape key
-		exit(1);
-	}
-    myDisplay();
-}
+//void myKeyboardFunc( unsigned char key, int x, int y ){}
 
 void mySpecialKeyFunc( int key, int x, int y )
 {
 	switch ( key ) {
         case GLUT_KEY_UP:
-            sl.add();
+            snake.add();
             break;
         case GLUT_KEY_DOWN:
-            sl.cutTail();
+            snake.cutTail();
             break;
 
         case GLUT_KEY_LEFT:
-            std::cout << sl.toString();
+            std::cout << snake.toString();
             throw std::exception();
-            break;
         default:
             break;
     }
@@ -235,22 +239,21 @@ int main(int argc, char ** argv)
 
 	// Sljedeci blok sluzi kao bugfix koji je opisan gore
 	#ifdef LINUX_UBUNTU_SEGFAULT
-        //ss2
-        int i=pthread_getconcurrency();
+        //int i=pthread_getconcurrency();
 	#endif
 
 	glutInitContextVersion(3, 3);
 	glutInitContextProfile(GLUT_CORE_PROFILE);
 	glutInitContextFlags(GLUT_DEBUG);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH );
-	glutInitWindowSize(sub_width,sub_height);
+	glutInitWindowSize((int)sub_width,(int)sub_height);
 	glutInitWindowPosition(100,100);
 	glutInit(&argc, argv);
 
 	window = glutCreateWindow("SimpleAnim" );
 	glutReshapeFunc(resizeWindow);
 	glutDisplayFunc(myDisplay);
-	glutKeyboardFunc( myKeyboardFunc );			// Handles "normal" ascii symbols
+//	glutKeyboardFunc( myKeyboardFunc );			// Handles "normal" ascii symbols
     //glutMouseFunc(myMouseFunc);
     glutPassiveMotionFunc(glutPassiveMotionFunc);
 	glutSpecialFunc( mySpecialKeyFunc );		// Handles "special" keyboard keys
@@ -280,11 +283,11 @@ bool init_data()
     //    glHint(GL_POLYGON_SMOOTH, GL_DONT_CARE);
     //    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    for (int j=0 ;j < 3 ; j++) sl.add();
+    for (int j=0 ;j < 3 ; j++) snake.add();
 
 	std::cout << "Going to load programs... " << std::endl << std::flush;
 
-	s.load_shaders({
+	shader_snake.load_shaders({
             "SnakeVertexShader.vert",
             "SnakeFragmentShader.frag",
             "" ,
@@ -292,7 +295,7 @@ bool init_data()
             "SnakeTessEval.tese"
     });
 
-    s_background.load_shaders({
+    shader_background.load_shaders({
           "BackgroundVertexShader.vert",
           "BackgroundFragmentShader.frag",
           "BackgroundGeometryShader.geom" ,
@@ -307,41 +310,60 @@ bool init_data()
 
 
 
-Renderer r;
+
 void myDisplay() {
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    sl.changeDirection(glm::degrees(glm::atan(mousePos.y,mousePos.x)));
-    sl.hop();
+    snake.changeDirection(glm::degrees(glm::atan(mousePos.y, mousePos.x)));
+    snake.hop();
 
-    const std::vector<GLfloat> my_data = sl.getCoords();
-    const std::vector<GLfloat> eye{
+    const std::vector<GLfloat> snake_points = snake.getCoords();
+    const std::vector<GLfloat> eyes_points{
                     0.02,   0.0,    0,  1/70.0, 400,
                     0.0,    0.0,    0,  1/50.0, 300 };
-    const std::vector<GLfloat> background_data = b.update(sl.head());
+    const std::vector<GLfloat> background_points = background.update(snake.head());
 
 
-    //first eye
-    s.setInt("eyes" , 1);
-    glm::mat4 mvp = createMVPEye(glm::vec3(0.1, 0.1, 0));
-    r.render(s,4,1,eye,mvp);
 
-    //second eye
-    mvp = createMVPEye(glm::vec3(0.1, -0.1, 0));
-    s.setInt("eyes" , 1);
-    // could be using this, setupData doesnt need to be used twice
-    //r.draw(mvp,2,s,2);
-    r.render(s,4,1,eye,mvp);
+    /**
+     * Eyes
+     * We have two MVPs. One eye is 'copied' twice to the scene, on different coordinates.
+     *
+     * For eyes ,we use SnakeShader which requires data organised like this:
+     *         I.attribute    |   II.attribute
+     *     Position     Scale |     Color
+     *     x y z        s     |     c
+     *
+     *     => positions = (4,1)
+     */
+    std::vector<glm::mat4> MVPs;
+    shader_snake.setInt("eyes" , 1);
+    MVPs.push_back(createMVPEye(glm::vec3(0.1, 0.1, 0)));
+    MVPs.push_back(createMVPEye(glm::vec3(0.1, -0.1, 0)));
+    Renderer::render(shader_snake, std::vector<int>({4, 1}) , eyes_points, MVPs);
 
-    //body
-    mvp = createMVPBody();
-    s.setInt("eyes" , 3);
-    r.render(s,4,1,my_data,mvp);
+    /**
+     * Body
+     * Data organisation same as above.
+     */
+    MVPs.clear();
+    MVPs.push_back(createMVPBody());
+    shader_snake.setInt("eyes" , 3);
+    Renderer::render(shader_snake, std::vector<int>({4, 1}) , snake_points, MVPs);
 
-    //background
-    mvp = createMVPBody();
-    r.render(s_background,3,0,background_data,mvp);
+    /**
+     * Background
+     * We use BackgroundShader which requires our data to be organised like this:
+     *         I.attribute    |
+     *          Position      |
+     *           x y z        |
+     *
+     *           => positions = (3)
+     */
+    MVPs.clear();
+    MVPs.push_back(createMVPBody());
+    Renderer::render(shader_background, std::vector<int>({3}) , background_points, MVPs);
 
 
     glutSwapBuffers();
